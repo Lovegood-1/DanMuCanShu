45e1cd80827cbb70a70bb042a59991acb4552510
Lovegood-1 <yinzepingchd@163.com>
2022-06-01 08:36:51 +0800

    f

--- test_Video.py
+++ test_Video.py
@@ -5,3 +5,4 @@ left_video = r'video\camera_rgb_double.avi'
 config = stereoconfig.stereoCamera()
 Video = video_rectify_double(left_video,  config)
 Video.rectify_video_double()
+Video.triangulation()
