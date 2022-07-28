355dca2620f90cc2371060b4289d313dd6db6e05
Lovegood-1 <yinzepingchd@163.com>
2022-07-28 18:54:56 +0800

    yolo多视频后处理完成

--- detect_api2.py
+++ detect_api2.py
@@ -13,7 +13,7 @@ from numpy import random
 from models.experimental import attempt_load
 from utils.datasets import LoadStreams, LoadImages
 from utils.general import (
-    build_targets, check_img_size, non_max_suppression, apply_classifier, scale_coords, xyxy2xywh, xywh2xyxy, plot_one_box, strip_optimizer)
+    build_targets, check_img_size, non_max_suppression, apply_classifier, scale_coords, xyxy2xywh, xywh2xyxy, plot_one_box, strip_optimizer, is_ip)
 from utils.torch_utils import select_device, load_classifier, time_synchronized
 """
 创建时间：20211202
@@ -78,7 +78,6 @@ def detect2(path_video = "inference\\images\\2.mp4,inference\\images\\1.mp4" ,sa
         # save_img = False
         dataset = LoadImages(source, img_size=imgsz)
     # 建立字典，并初始化
-    dict_all = {}
     dict_ = {}  
     dict_[0] = {}
     dict_[1] = {}
@@ -192,8 +191,207 @@ def detect2(path_video = "inference\\images\\2.mp4,inference\\images\\1.mp4" ,sa
     print('Done. (%.3fs)' % (time.time() - t0))
     return dict_[0], dict_[1]
 
-def get_max_bbox(dict_):
-    pass
+
+
+def detect_multi_videos(video_info  ,save_img=True):
+    """从上面 detect2 复制，适用于多个视频的输入
+
+    Args:
+        path_video : 所有视频及其时间. Defaults to [{'name':0721, "one":path1; "two": path2}, {'name':0722, "one":path1; "two": path2}]
+        save_img (bool, optional): _description_. Defaults to True.
+
+    Returns:
+        _type_: [{"one":{"path": d:/, "info": { "fire_info"}}; "two":{"path": d:/, "info": { "fire_info"}}；} }, 
+                 {'name':0722, "one":path1; "two": path2}]
+    """
+
+    """需要修改
+
+    这个函数的目的是修改 path_video，你最好备份一下变量。
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='best.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--output', type=str, default='inference\\output', help='output folder')  # output folder
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.4, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.5, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='0', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    opt = parser.parse_args()
+    opt.weights = 'best.pt'
+    opt.source = video_info  
+    opt.save_txt = True
+
+
+
+    out, source, weights, view_img, save_txt, imgsz = \
+        opt.output, opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size
+    webcam = False
+
+    # Initialize
+    device = select_device(opt.device)
+    if os.path.exists(out):
+        shutil.rmtree(out)  # delete output folder
+    os.makedirs(out)  # make new output folder
+    half = device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model
+    model = attempt_load(weights, map_location=device)  # load FP32 model
+    imgsz = check_img_size(imgsz, s=model.stride.max())  # check img_size
+    if half:
+        model.half()  # to FP16
+
+    # Second-stage classifier
+    classify = False
+    if classify:
+        modelc = load_classifier(name='resnet101', n=2)  # initialize
+        modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model'])  # load weights
+        modelc.to(device).eval()
+    names = model.module.names if hasattr(model, 'module') else model.names
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(names))]
+    # 需要修改的第一处 ：
+    """
+    需要修改的第一处
+    这里的 source 应该可以传单个视频的路径，你需要依据多个路径，循环创建 dataset，然后把检测结果放到 dict中
+    """
+    # Set Dataloader
+
+    """
+    需要修改的第二处：原来我们是一下处理2视频，现在不需要了，你就建立一个字典就好
+        dict_ = {}  
+    dict_[0] = {} # 放第一个视频的帧 以及火焰 {帧数：BBOX}
+    dict_[1] = {} # 放第二个视频的帧 以及火焰 {帧数：BBOX}  
+    """
+    for _time in video_info:
+        # numbers : dict
+        # 遍历所有IP 
+        for k ,v in _time.items():
+            if is_ip(k.strip()) is False:
+                continue
+            print(_time['time'], k)
+            _video_path = _time[k]['path']
+            dict_ = {}  
+            dict_[0] = {}
+            dict_[1] = {}
+            source = _time[k]['path']
+            vid_path, vid_writer = None, None
+            if webcam:
+                view_img = True
+                cudnn.benchmark = True  # set True to speed up constant image size inference
+                dataset = LoadStreams(source, img_size=imgsz)
+            else:
+                # save_img = False
+                dataset = LoadImages(source, img_size=imgsz)
+            # 建立字典，并初始化
+
+            # Run inference
+            t0 = time.time()
+            img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
+            _ = model(img.half() if half else img) if device.type != 'cpu' else None  # run once
+            for path, img, im0s, vid_cap in dataset:
+                
+                img = torch.from_numpy(img).to(device)
+                img = img.half() if half else img.float()  # uint8 to fp16/32
+                img /= 255.0  # 0 - 255 to 0.0 - 1.0
+                if img.ndimension() == 3:
+                    img = img.unsqueeze(0)
+
+                # Inference
+                t1 = time_synchronized()
+                with torch.no_grad():
+                    pred = model(img, augment=opt.augment)[0]
+
+                # Apply NMS
+                pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+                t2 = time_synchronized()
+
+                # Apply Classifier
+                if classify:
+                    pred = apply_classifier(pred, modelc, img, im0s)
+
+                # Process detections
+                for i, det in enumerate(pred):  # detections per image
+                    if webcam:  # batch_size >= 1
+                        p, s, im0 = path[i], '%g: ' % i, im0s[i].copy()
+                    else:
+                        p, s, im0 = path, '', im0s
+
+                    save_path = str(Path(out) / Path(p).name)
+                    txt_path = str(Path(out) / Path(p).stem) + ('_%g' % dataset.frame if dataset.mode == 'video' else '')
+                    
+                    s += '%gx%g ' % img.shape[2:]  # print string
+                    gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+                    if det is not None and len(det):
+                        # Rescale boxes from img_size to im0 size
+                        det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                        # Print results
+                        for c in det[:, -1].unique():
+                            n = (det[:, -1] == c).sum()  # detections per class
+                            s += '%g %ss, ' % (n, names[int(c)])  # add to string
+
+                        # Write results
+                        for *xyxy, conf, cls in det:
+                            if cls  != 0:
+                                continue
+                            if save_txt:  # Write to file
+                                xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                                if dataset.frame in dict_[dataset.count]:
+                                    dict_[dataset.count][dataset.frame].append(('%g ' * 5 ) % (cls, *xywh))
+                                else:
+                                    dict_[dataset.count][dataset.frame] = []
+                                    dict_[dataset.count][dataset.frame].append(('%g ' * 5 ) % (cls, *xywh))
+
+                                # with open(txt_path + '.txt', 'a') as f:
+                                #     f.write(('%g ' * 5 + '\n') % (cls, *xywh))  # label format
+
+                            if save_img or view_img:  # Add bbox to image
+                                label = '%s %.2f' % (names[int(cls)], conf)
+                                plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
+                                # plot_one_box(xyxy, im0, label=None, color=colors[int(cls)], line_thickness=3)  # 只画框，不画类别 置信度
+                    # Print time (inference + NMS)
+                    t3 = time.time()
+                    print('%sDone. (%.3fs); All:(%.3fs)' % (s, t2 - t1, t3 - t1))
+
+                    # Stream results
+                    # if view_img:
+                    #     cv2.imshow(p, im0)
+                    #     if cv2.waitKey(1) == ord('q'):  # q to quit
+                    #         raise StopIteration
+
+                    # Save results (image with detections)
+                    if save_img:
+                        if dataset.mode == 'images':
+                            cv2.imwrite(save_path, im0)
+                        else:
+                            if vid_path != save_path:  # new video
+                                vid_path = save_path
+                                if isinstance(vid_writer, cv2.VideoWriter):
+                                    vid_writer.release()  # release previous video writer
+
+                                fourcc = 'mp4v'  # output video codec
+                                fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                                if fps > 100:
+                                    fps = 30
+                                w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                                h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                                vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*fourcc), fps, (w, h))
+                            vid_writer.write(im0)
+                    # print('%sDone.   (%.3fs)' % (s,  time.time() - t1))
+                # index_of_video = dataset.count
+                """
+                需要修改的地方
+                这里可以修改变量了
+                """
+            _time[k]['info']["dict_all_fire"] = dict_[0]
+    print('Done. (%.3fs)' % (time.time() - t0))
+    return video_info
 
 
 if __name__ == '__main__':
@@ -220,18 +418,34 @@ if __name__ == '__main__':
                 detect2()
                 strip_optimizer(opt.weights)
         else:
-            dict_, dict_2 = detect2("video\\camera_leftrectify.avi,video\\camera_rightrectify.avi")
-            # Max_area_bbox = -1
-            # Max_area_bbox_index = -1
-            # d = dict_
-            # new_d_area = {}
-            # for num_frame in d:
-            #     bbox_list = d[118][0].strip().split(' ')[1:]
-            #     bbox =  [float(i) for i in bbox_list]
-            #     new_d_area[num_frame] = bbox[2] * bbox[3]
-            #     if bbox[2] * bbox[3] > Max_area_bbox:
-            #         Max_area_bbox = bbox[2] * bbox[3]
-            #         Max_area_bbox_index = num_frame
+            video_info =             [{'time':'20220727_111040', 
+                '192.168.1.1':
+                    {"path": r'video_new\20220727_111040\192.168.1.1.avi', 
+                    "info": { "dict_all": 1,  # 这个是火焰检测负责计算
+                                "dict1_first_fire": 1,
+                                "missle_info":1} ,    # 这个导弹检测计算
+                    },
+                '192.168.1.2':
+                    {"path": r'video_new\20220727_111040\192.168.1.2.avi', 
+                    "info": { "dict_all": 1, # 这个是火焰检测负责计算
+                                "dict1_first_fire":1,
+                                "missle_info":1}     # 这个导弹检测计算
+                                }},
+                                {'time':'20220727_111050', 
+                '192.168.1.1':
+                    {"path": r'video_new\20220727_111050\192.168.1.1.avi', 
+                    "info": { "dict_all": 1,  # 这个是火焰检测负责计算
+                                "dict1_first_fire": 1,
+                                "missle_info":1} ,    # 这个导弹检测计算
+                    },
+                '192.168.1.2':
+                    {"path": r'video_new\20220727_111050\192.168.1.2.avi', 
+                    "info": { "dict_all": 1, # 这个是火焰检测负责计算
+                                "dict1_first_fire":1,
+                                "missle_info":1}     # 这个导弹检测计算
+                                }}]  
+            dict_, dict_2 = detect_multi_videos(video_info)
+            
     sorted_results = sorted(dict_.items(), key = lambda kv:(kv[1], kv[0]), reverse=True)
     Max_area_bbox_index = sorted_results[1][0]
     # Max_area_bbox = sorted_results[1][-1][0].strip().split(' ')[1:]
--- post_process.py
+++ post_process.py
@@ -1,19 +1,33 @@
+
+import re
+import os
+import datetime
+import glob
+from detect_api2 import detect_multi_videos
+def check_string(re_exp, str):
+    res = re.search(re_exp, str)
+    if res:
+        return True
+    else:
+        return False
 class V:
     def __init__(self, video_dir) -> None:
         self.video_dir = video_dir
-        self.video_info # 列表，[{'name':0721, "one":{"path": d:/, "info": { "fire_info"}}; "two":{"path": d:/, "info": { "fire_info"}}；}]
+        # self.video_info # 列表，[{'name':0721, "one":{"path": d:/, "info": { "fire_info"}}; "two":{"path": d:/, "info": { "fire_info"}}；}]
         self.search_video_info()
         pass
 
     def post_process(self):
         # 1 多视频火焰检测
-
+        print("1 启动yolo后处理")
+        self.video_info = detect_multi_videos(self.video_info)
 
         pass
 
     def search_video_info(self):
-        """初始化视频列表
-            [{'time':20220727_111040, 
+        """
+        初始化视频列表
+            [{'time':'20220727_111040', 
               '192.168.1.1':
                     {"path": d:/, 
                     "info": { "dict_all": dict;  # 这个是火焰检测负责计算
@@ -26,8 +40,34 @@ class V:
                               "missle_info":}     # 这个导弹检测计算
                               }]  
         """
-        pass
+        videos_info = []
+        videos_path = self.video_dir
+        # 1 初始化时间并加入
+        _today = datetime.datetime.now()
+        time_list = os.listdir(videos_path) 
+        for time_ in time_list: # 如果符合命名规则而且是今天则加入
+            # if  check_string('^[0-9]{8}_[0-9]{6}$',time_)  and datetime.datetime.strptime(time_,'%Y%m%d_%H%M%S').date() == _today.date():
+            if  check_string('^[0-9]{8}_[0-9]{6}$',time_):
 
-    
-video_dir = r''
+                videos_info.append({'time':str(time_)})
+                pass
+        
+        # 2 获取每个时间段的视频
+        for time_ in videos_info:
+            time_dir = os.path.join(videos_path , time_['time']) 
+            fileExtensions = [ "avi","mp4" ]
+            videos_list = []
+            for extension in fileExtensions: # 获取所有视频的相对路径
+                videos_list.extend( glob.glob( time_dir + '\\*.' + extension  ))
+
+            for v in videos_list: # 查看是否所有视频的命名都符合规则: 必须存在 ip
+                if len(re.findall( r'[0-9]+(?:\.[0-9]+){3}',v)) > 0:
+                    time_[re.findall( r'[0-9]+(?:\.[0-9]+){3}',v)[0]] = {}  # ip 作为键 
+                    time_[re.findall( r'[0-9]+(?:\.[0-9]+){3}',v)[0]]['path'] = v
+                    time_[re.findall( r'[0-9]+(?:\.[0-9]+){3}',v)[0]]['info'] = {}
+        self.video_info = videos_info
+
+video_dir = "video_new"
 v = V(video_dir)
+v.post_process()
+a = 1
